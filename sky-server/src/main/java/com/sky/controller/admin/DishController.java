8fe81d28c1482b29e34fc84a2ad7b4f50bb5e52a
CaseyWon <CaseyWon@outlook.com>
2024-02-23 12:53:07 +0800

    删除菜品

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/dish")
 @Slf4j
@@ -28,5 +30,14 @@ public class DishController {
     public Result<PageResult> page (DishPageQueryDTO dishPageQueryDTO){
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+    /*
+    * 菜品批量删除
+    * */
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids){
+        dishService.deleteBatch(ids);
+
+        return Result.success();
     }
 }
