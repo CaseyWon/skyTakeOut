2961ba2fa471b0cfe3647c949d3f5c610dd758b9
CaseyWon <CaseyWon@outlook.com>
2024-02-26 13:27:04 +0800

    修改套餐业务

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -15,8 +15,6 @@ public interface SetmealMapper {
 
     /**
      * 根据分类id查询套餐的数量
-     * @param id
-     * @return
      */
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
@@ -34,8 +32,6 @@ public interface SetmealMapper {
 
     /**
      * 根据id查询套餐
-     * @param id
-     * @return
      */
     @Select("select * from setmeal where id = #{id}")
     Setmeal getById(Long id);
@@ -45,4 +41,10 @@ public interface SetmealMapper {
      */
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
+
+    /*
+    * 修改套餐
+    * */
+    @AutoFill(OperationType.UPDATE)
+    void update(Setmeal setmeal);
 }
--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
 
 import java.util.List;
 
@@ -21,4 +22,14 @@ public interface SetmealService {
      * 批量删除套餐
      */
     void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询套餐和关联的菜品数据
+     */
+    SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 修改套餐
+     */
+    void update(SetmealDTO setmealDTO);
 }
