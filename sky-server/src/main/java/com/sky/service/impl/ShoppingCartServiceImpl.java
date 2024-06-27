adf01bae2641b5b6fbeca864e16735cfec4e425a
CaseyWon <CaseyWon@outlook.com>
2024-06-27 17:45:44 +0800

    查看购物车功能

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -62,4 +62,13 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         }
 
     }
+
+    //查看购物车
+    public List<ShoppingCart> showShoppingCart() {
+        //获取用户id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder().userId(userId).build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
